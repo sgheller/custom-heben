aebf9e75f08bb6cfd541ac3a39d870261350bfad
Georgina Guzman <georginaguzman267@gmail.com>
2021-07-20 09:15:22 -0300

    [ADD][product_seasons] Incidence nbr./s.: ODOO-1630. - Add modulo for management product seasons

---
+++ product_seasons/models/product_seasons.py
@@ -0,0 +1,31 @@
+from odoo import api, fields, models
+
+
+class ProductSeasons(models.Model):
+    _name = "product.seasons"
+    _description = "Product Seasons"
+    _order = "name"
+
+    name = fields.Char("Season Name", Required=True)
+    description = fields.Text(translate=True)
+    product_ids = fields.One2many(
+    "product.template", "product_seasons_id", string="Season Products"
+    )
+    products_count = fields.Integer(
+        string="Number of products", compute="_compute_products_count"
+    )
+
+    @api.depends("product_ids")
+    def _compute_products_count(self):
+        product_model = self.env["product.template"]
+        groups = product_model.read_group(
+            [("product_seasons_id", "in", self.ids)],
+            ["product_seasons_id"],
+            ["product_seasons_id"],
+            lazy=False,
+        )
+        data = {group["product_seasons_id"][0]: group["__count"] for group in groups}
+        for seasons in self:
+            seasons.products_count = data.get(seasons.id, 0)
+
+    logo = fields.Binary("Logo File")
