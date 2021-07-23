a5b4d901367163db36e28876748bef0c4bc8538f
Georgina Guzman <georginaguzman267@gmail.com>
2021-07-23 16:23:21 -0300

    [ADD][product_inofrmation] Incidence nbr./s.: ODOO-1699. - Add field

--- product_information/models/product_template.py
+++ product_information/models/product_template.py
@@ -3,5 +3,5 @@ from odoo import models, fields
 class ProductTemplate (models.Model):
     _inherit = 'product.template'
 
-    country_id = fields.Many2one(string="Country", comodel_name='res.country')
-    maker_id = fields.Many2one('res.partner', 'Marker', domain=[('customer_rank','=',0)])
+    country_id = fields.Many2one(string="País", comodel_name='res.country')
+    maker_id = fields.Many2one('res.partner', 'Fabricante', domain=[('customer_rank','=',0)])
