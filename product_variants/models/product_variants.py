b0503dc86946d736d346cee99b875fbe06143af1
Andres Andrade <andres.63.d@gmail.com>
2021-10-29 08:59:18 -0300

    [UPD][product_variants] Incidence nbr./s.: ODOO-1983. - Add activity mark fields - Views and translations

--- product_variants/models/product_variants.py
+++ product_variants/models/product_variants.py
@@ -13,28 +13,28 @@ class ProductProduct (models.Model):
     last_sale_date = fields.Char(string="Fecha de Última Venta", compute="_compute_date_last_sale")
 
     def _compute_date_first_entry(self):
-        record = self.env['stock.move.line'].search([('product_id', '=', self.id),('picking_code', '=', 'incoming'),('state', '=', 'done')], order='date desc', limit=1)
+        record = self.env['stock.move.line'].search([('product_id', '=', self.id),('picking_code', '=', 'incoming'),('state', '=', 'done')], order='date asc', limit=1)
         if record.date:
             self.first_entry_date = str(record.date)[:11]
         else:
             self.first_entry_date = ""
 
     def _compute_date_firts_sale(self):
-        record = self.env['stock.move.line'].search([('product_id', '=', self.id),('picking_code', '=', 'outgoing'),('state', '=', 'done'),('origin', '!=', False)], order='date desc', limit=1)
+        record = self.env['stock.move.line'].search([('product_id', '=', self.id),('picking_code', '=', 'outgoing'),('state', '=', 'done'),('origin', '!=', False)], order='date asc', limit=1)
         if record.date:
             self.first_sale_date = str(record.date)[:11]
         else:
             self.first_sale_date = ""
 
     def _compute_date_last_sale(self):
-        record = self.env['stock.move.line'].search([('product_id', '=', self.id),('picking_code', '=', 'outgoing'),('state', '=', 'done'),('origin', '!=', False)], order='date asc', limit=1)
+        record = self.env['stock.move.line'].search([('product_id', '=', self.id),('picking_code', '=', 'outgoing'),('state', '=', 'done'),('origin', '!=', False)], order='date desc', limit=1)
         if record.date:
             self.last_sale_date = str(record.date)[:11]
         else:
             self.last_sale_date = ""
 
     def _compute_date_last_entry(self):
-        record = self.env['stock.move.line'].search([('product_id', '=', self.id),('picking_code', '=', 'incoming'),('state', '=', 'done')], order='date asc', limit=1)
+        record = self.env['stock.move.line'].search([('product_id', '=', self.id),('picking_code', '=', 'incoming'),('state', '=', 'done')], order='date desc', limit=1)
         if record.date:
             self.last_entry_date = str(record.date)[:11]
         else: 
