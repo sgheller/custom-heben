659047c8acbd31fef90508e5666dd7976660f4cc
gabbii <gperez@calyxservicios.com.ar>
2022-01-26 08:47:59 -0300

    [FIX][pos_seller]ODOO-2929. - FIX field many2many relation employee

--- pos_seller/models/pos_config.py
+++ pos_seller/models/pos_config.py
@@ -5,8 +5,9 @@ from odoo import models, fields, _
 class PosConfig(models.Model):
     _inherit = 'pos.config'
     
-    module_pos_seller = fields.Boolean(help=_("Show seller PoS sesion"))
+    module_pos_seller = fields.Boolean(help=_("Show seller PoS session"))
     seller_ids = fields.Many2many(
-        'hr.employee', string=_("Seller with access"),
+        'hr.employee', 'hr_employee_seller_pos', 
+        string=_("Seller with access"),
         help=_("If left empty, all sellers can be used in the POS session"))
 
